96f161d637a9f9040ec6195422fff21e141c13a0
TimeATronics <aradhya.chakrabarti@gmail.com>
2025-04-02 19:24:45 +0530

    Initial Structure

--- ans1/src/answer1/main.go
+++ ans1/src/answer1/main.go
@@ -1,5 +1,25 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Author: Aradhya Chakrabarti
+// Roll No: 2205880
+// Description: This program fetches numbers from external APIs and maintains a sliding window to compute the moving average.
+
+// APIResponse struct to store the response from the external number API
+type APIResponse struct {
+	Numbers []int `json:"numbers"`
+}
+
+func numberHandler(w http.ResponseWriter, r *http.Request) {}
+
+// Main function to start the HTTP server
 func main() {
-    fmt.Println("hello world")
+	http.HandleFunc("/numbers/", numberHandler)
+	fmt.Println("Server running on :9876")
+	log.Fatal(http.ListenAndServe(":9876", nil))
 }
